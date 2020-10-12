50956dcc88140b3f8d8fbc527e1d1a5b05ffa199
TheUltimateKeval <32948647+TheUltimateKeval@users.noreply.github.com>
2020-10-12 15:13:58 +0530

    BOOO!!!!!!
    1) Added a jumpscare when logo is clicked.
    2) Minor CSS changes to the Header

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const logo_img = document.getElementById("header-logo-img");
+const jumpscare_div = document.getElementsByClassName("jumpscare")[0];
 
 logo_img.addEventListener("mouseover", () => {
     logo_img.src = "./img/logo.gif";
@@ -6,4 +7,13 @@ logo_img.addEventListener("mouseover", () => {
 
 logo_img.addEventListener("mouseleave", () => {
     logo_img.src = "./img/logo.png";
+});
+
+logo_img.addEventListener("click", () => {
+    jumpscare_div.style.display = "block";
+    jumpscare_div.style.animation = "jumpscareFadeOut 4s";
+    setTimeout(() => {
+        jumpscare_div.style.animation = "";
+        jumpscare_div.style.display = "none";
+    }, 4000);
 });
