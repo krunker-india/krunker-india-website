2b2d9d5463176c1d5b6080cd18e70cc26d1bee34
TheUltimateKeval <32948647+TheUltimateKeval@users.noreply.github.com>
2020-10-12 16:50:40 +0530

    Added a jumpscare sound

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 const logo_img = document.getElementById("header-logo-img");
-const jumpscare_div = document.getElementsByClassName("jumpscare")[0];
 
 logo_img.addEventListener("mouseover", () => {
     logo_img.src = "./img/logo.gif";
@@ -9,7 +8,13 @@ logo_img.addEventListener("mouseleave", () => {
     logo_img.src = "./img/logo.png";
 });
 
+
+const jumpscare_div = document.getElementsByClassName("jumpscare")[0];
+const jumpscare_sound = new Audio("./sounds/jumpscare.mp3");
+
 logo_img.addEventListener("click", () => {
+    jumpscare_sound.currentTime = 0.4;
+    jumpscare_sound.play();
     jumpscare_div.style.display = "block";
     jumpscare_div.style.animation = "jumpscareFadeOut 4s";
     setTimeout(() => {
