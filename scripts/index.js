b44e3113973328949f30175332da86ab2c98350f
TheUltimateKeval <32948647+TheUltimateKeval@users.noreply.github.com>
2020-10-13 00:13:23 +0530

    Totally made the website mobile friendly
    1) Added an overlay for portrait mode
    2) Removed unnecessary rubbish from the CSS
    3) Added comments in `index.js`

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,23 @@
+// Making the website friendly :kappa:
+
+const windowSizeOverlay = document.getElementById("window-size-overlay");
+
+window.addEventListener("load", checkWindowSize);
+window.addEventListener("resize", checkWindowSize);
+
+function checkWindowSize(){
+    var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+    if(w > h && (windowSizeOverlay.style.display == "" || windowSizeOverlay.style.display == "block")) {
+        windowSizeOverlay.style.display = "none";
+    } else {
+        windowSizeOverlay.style.display = "block";
+    }
+}
+
+// Header Logo animation on hover
+
 const logo_img = document.getElementById("header-logo-img");
 
 logo_img.addEventListener("mouseover", () => {
@@ -8,6 +28,7 @@ logo_img.addEventListener("mouseleave", () => {
     logo_img.src = "./img/logo.png";
 });
 
+// Jumpscare
 
 const jumpscare_div = document.getElementsByClassName("jumpscare")[0];
 const jumpscare_sound = new Audio("./sounds/jumpscare.mp3");
